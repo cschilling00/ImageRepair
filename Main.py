f81cfa82be326d3c6ccc90e48835cf9733ff2a48
Cassandra <cassandra.schilling00@gmail.com>
2022-01-29 22:28:35 +0100

    added damaged image and made first image analysis

---
+++ Main.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import numpy as np
+
+
+# load color image
+def load_image():
+    img_path = 'SAMSUNG SM-J710F (000014600640).jpg'
+    image = cv.imread(img_path, cv.IMREAD_COLOR)
+
+
+
+def compare_pixel_row():
+    img_path = 'SAMSUNG SM-J710F (000014600640).jpg'
+    image = cv.imread(img_path, cv.IMREAD_COLOR)
+
+
+
+load_image()
